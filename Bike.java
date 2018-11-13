0fbf9335044848a397f63c78faa8a1a91ddd69d0
Youneselfeitori <31775550+Youneselfeitori@users.noreply.github.com>
2018-11-13 14:34:26 -0500

    Create Bike.java

---
+++ Bike.java
@@ -0,0 +1,17 @@
+/****************************************************************************
+ *
+ * Created by: Younes
+ * Created on: Nov 2018
+ * Created for: ICS4U
+ * Bike (motorcycle) subclass (child class) of Vehicle class for vehicle program
+ *
+ ****************************************************************************/
+
+public class Bike extends Vehicle {
+	
+	public Bike(String license,
+				String colour) {
+		
+		super(license, colour, 2); //inherits properties from superclass constructor
+	}
+}
